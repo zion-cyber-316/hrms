a8a1b4a6db242a916e715c887cb13af10a4197a9
Gaurav mehra <gaurav9759434559@gmail.com>
2025-11-24 06:04:28 +0530

    Update login API endpoint and remove comments

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -15,7 +15,7 @@ const handleSubmit = async(e)=>{
     e.preventDefault()
  try{
    
-    const response = await axios.post("http://localhost:5000/auth/login",{email,password});
+    const response = await axios.post("https://hrms-backend-hjoj.onrender.com/auth/login",{email,password});
 if(response.data.success){
   login(response.data.user)
   localStorage.setItem('token',response.data.token)
@@ -133,78 +133,6 @@ export default Login
 
 
 
-// import React, { useState } from 'react';
-// import axios from "axios";
-// import { useAuth } from '../Context/AuthContext';
-// import { useNavigate } from 'react-router-dom';
 
-// const Login = () => {
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-//   const { login } = useAuth();
-//   const Navigate = useNavigate();
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const response = await axios.post("http://localhost:5000/auth/login", { email, password });
-
-//       if (response.data.success) {
-//         const user = response.data.user;
-//         const token = response.data.token;
-
-//         // 🔥 Role-based token store
-//         if (user.role === "admin") {
-//           localStorage.setItem("adminToken", token);
-//         } else {
-//           localStorage.setItem("employeeToken", token);
-//         }
-
-//         // AuthContext me user set karo
-//         login(user);
-
-//         // Redirect according to role
-//         if (user.role === "admin") {
-//           Navigate("/admin-dashbord");
-//         } else {
-//           Navigate("/employee-dashbord");
-//         }
-//       }
-
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   }
-
-//   return (
-//     <div className='flex flex-col items-center h-screen justify-center bg-gradient-to-b from-teal-600 from-50% from-gray-100 to-50%  space-y-6'>
-//       <h1 className='font-sevillana text-3xl text-white'> Employees Management System</h1>
-
-//       <div className='border shadow p-6 w-88 bg-white'>
-//         <h2 className='text-2xl font-bold mb-4'>Login Form</h2>
-
-//         <form onSubmit={handleSubmit}>
-//           <div className='mb-4'>
-//             <label className='block text-gray-700' htmlFor="email">Email</label>
-//             <input className='w-full px-3 py-2 border' type='email'
-//               placeholder='Enter your Email'
-//               onChange={(e) => setEmail(e.target.value)} />
-//           </div>
-
-//           <div className='mb-4'>
-//             <label className='block text-gray-700' htmlFor="Password">Password</label>
-//             <input className='w-full px-3 py-2 border' type='password'
-//               placeholder='*********'
-//               onChange={(e) => setPassword(e.target.value)} />
-//           </div>
-
-//           <button type='submit' className='w-full bg-teal-600 text-white py-2'>
-//             Login
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   )
-// }
 
 // export default Login;
